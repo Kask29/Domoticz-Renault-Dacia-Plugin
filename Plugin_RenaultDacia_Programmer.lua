6cc321833a3f50e3dc6d2187558e1ab1584ebbd0
Kask29 <98609356+Kask29@users.noreply.github.com>
2023-04-01 14:42:31 +0200

    Update Plugin_RenaultDacia_Programmer.lua

--- Plugin_RenaultDacia_Programmer.lua
+++ Plugin_RenaultDacia_Programmer.lua
@@ -18,7 +18,7 @@ return {
 	execute = function(domoticz, triggeredItem)
         if (triggeredItem.trigger == 'plugin_Deprogrammer_charge_RenaultDacia') then
             local splittedResult = domoticz.utils.stringSplit(triggeredItem.data,'---')
-            domoticz.log("Annulation des programmation pour idx " .. splittedResult[1] .. " et " .. splittedResult[2])
+            domoticz.log("Annulation des programmation pour les idx " .. splittedResult[1] .. " et " .. splittedResult[2] .. " pour " .. splittedResult[7])
             domoticz.devices(tonumber(splittedResult[1])).cancelQueuedCommands()
             domoticz.devices(tonumber(splittedResult[2])).cancelQueuedCommands()
         elseif (triggeredItem.trigger == 'plugin_Programmer_charge_RenaultDacia') then
@@ -31,7 +31,7 @@ return {
             domoticz.devices(tonumber(splittedResult[2])).switchOff().at(splittedResult[5] .. " on " .. splittedResult[6])
         else
             local splittedResult = domoticz.utils.stringSplit(triggeredItem.data,'---')
-            domoticz.log("Annulation des programmation pour idx " .. splittedResult[1] .. " et " .. splittedResult[2])
+            domoticz.log("Annulation des programmation pour idx " .. splittedResult[1] .. " et " .. splittedResult[2] .. " pour " .. splittedResult[7])
             domoticz.devices(tonumber(splittedResult[1])).cancelQueuedCommands()
             domoticz.devices(tonumber(splittedResult[2])).cancelQueuedCommands()
         end
